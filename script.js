16d34be809ae222bebeef62d40f87bb4cd016932
arrafidainschule <redaalsaraf@yahoo.com>
2025-08-10 23:29:47 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hamburger-Menü Steuerung
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menu-icon");
+    const nav = document.querySelector("nav");
+
+    menuIcon.addEventListener("click", function() {
+        nav.classList.toggle("active");
+    });
+});
