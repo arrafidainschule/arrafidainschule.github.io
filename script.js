d0ce90682cce7e404a9512137433941ac75dc86c
arrafidainschule <redaalsaraf@yahoo.com>
2025-08-12 21:10:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("show");
 }
+
+// Hover-Effekt für Social-Icons
+document.querySelectorAll('.social-icons img').forEach(img => {
+  const originalSrc = img.src;
+  const hoverSrc = img.getAttribute('data-hover');
+
+  img.addEventListener('mouseenter', () => {
+    img.src = hoverSrc;
+  });
+
+  img.addEventListener('mouseleave', () => {
+    img.src = originalSrc;
+  });
+});
